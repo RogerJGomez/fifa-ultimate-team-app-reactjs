43625433eac158f21c4f346f9f204254292a6eeb
RogerJGomez <gomez.roger779@gmail.com>
2020-03-08 17:57:39 -0400

    Added link to personal website

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <h3 className="navbar-brand">Designed by: Roger Gómez</h3>
+            <h3 className="navbar-brand">Designed by: <a className="navbar-brand link" href="https://rogerjgomez.herokuapp.com/">Roger Gómez</a></h3>
         </div>
     )
 }
