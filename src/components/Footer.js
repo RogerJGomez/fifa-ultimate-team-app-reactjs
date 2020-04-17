bbbc13457c34dc8d2cd99e00470185261faf5f6f
RogerJGomez <gomez.roger779@gmail.com>
2020-04-16 21:17:05 -0400

    New portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer">
-            <h3 className="navbar-brand">Designed by: <a className="navbar-brand link" href="https://rogerjgomez.herokuapp.com/">Roger Gómez</a></h3>
+            <h3 className="navbar-brand">Designed by: <a className="navbar-brand link" href="https://rogergomez.netlify.app/">Roger Gómez</a></h3>
         </div>
     )
 }
