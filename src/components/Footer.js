76a64bb9613b2014de9d36d45bf6833810077e5c
Roger Gómez <gomez.roger779@gmail.com>
2020-06-20 13:43:52 -0400

    Updated portfolio link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <h3 className="navbar-brand">Designed by: <a className="navbar-brand link" href="https://rogergomez.netlify.app/">Roger Gómez</a></h3>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <h3 className="navbar-brand">
+        Designed by:{" "}
+        <a className="navbar-brand link" href="https://rogerjgomez.tech/">
+          Roger Gómez
+        </a>
+      </h3>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
